6ed222a78f0915646ed40a1a1d6137300530a2cb
Eskindir-stat260 <55426579+Eskindir-stat260@users.noreply.github.com>
2022-03-30 17:08:05 -0700

    Add files via upload

--- ui.R
+++ ui.R
@@ -1,3 +1,20 @@
+
+library(tidyverse)
+library(dplyr)
+(auto_mpg = ggplot2::mpg)
+sapply(auto_mpg,class)
+names(auto_mpg)
+auto_mpg = data.frame(auto_mpg)
+auto_mpg = auto_mpg %>% select(displ,year,cyl,cty,hwy)
+auto_mpg$displ <- as.numeric(as.integer(auto_mpg$displ))
+auto_mpg$year <- as.numeric(as.integer(auto_mpg$year))
+auto_mpg$cyl <- as.numeric(as.integer(auto_mpg$cyl))
+auto_mpg$cty <- as.numeric(as.integer(auto_mpg$cty))
+auto_mpg$hwy <- as.numeric(as.integer(auto_mpg$hwy))
+auto_mpg = na.omit(auto_mpg)
+any(is.na(auto_mpg[]))
+
+
 attrs = names(auto_mpg)
 
 library(shiny)
